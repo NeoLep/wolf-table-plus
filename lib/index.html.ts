74cdff5840ef2e652581f8847c41b345ecd0fc02
Leep <1197735110@qq.com>
2025-07-11 17:04:53 +0800

    fix: fix border styles

--- lib/index.html.ts
+++ lib/index.html.ts
@@ -32,7 +32,7 @@ export function toHtml(t: Table, from: string) {
         if (lineStyle === 'dashed' || lineStyle === 'dotted') {
             return `1px ${lineStyle} ${color}`
         } else {
-            return `${lineStyle === 'thick' ? 3 : lineStyle === 'medium' ? 2 : 1}pt solid ${color}`
+            return `${lineStyle === 'thick' ? 3 : lineStyle === 'medium' ? 2 : 1}px solid ${color}`
         }
     }
     const cellIndexes = new Map()
